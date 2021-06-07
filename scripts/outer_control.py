5b88c55dca8186c2ac0e8de29865fabcdcb9257d
chenhan0120 <stark.chen@connect.polyu.hk>
2021-06-07 17:51:10 +0800

    fix for straight path

--- scripts/outer_control.py
+++ scripts/outer_control.py
@@ -42,9 +42,10 @@ class UAVController(object):
     
     def vel_callback(self,vel):
         self.vel = np.array([vel.twist.linear.x,vel.twist.linear.y,vel.twist.linear.z])
-    def publish_bp(self,pts,t_points):
+    def publish_bp(self,pts,t_points,p_index):
         bpts = MarkerArray()
-        pts = np.r_[np.array(pts),np.array([t_points])]
+        len_pts = len(pts)
+        pts = np.r_[np.array(pts),np.array(t_points),np.array([t_points[min(p_index,len(t_points)-1)]])]
         for j in range(len(pts)):
             point=pts[j]
             bp = Marker()
@@ -69,7 +70,15 @@ class UAVController(object):
                 bp.color.a = 1
                 bp.color.r = 0.9
                 bp.color.g = 0.1
-                bp.color.b = 0.9           
+                bp.color.b = 0.9    
+            elif j > len_pts-1:
+                bp.scale.x = 0.4
+                bp.scale.y = 0.4
+                bp.scale.z = 0.4
+                bp.color.a = 1
+                bp.color.r = 0.6
+                bp.color.g = 1.0
+                bp.color.b = 0.0
             else:
                 bp.color.a = 1
                 bp.color.r = 0.6
@@ -78,7 +87,7 @@ class UAVController(object):
 
             bpts.markers.append(bp)
         self.bp_publisher.publish(bpts)
-    def publish_locwp(self,wp,wp2):
+    def publish_locwp(self,wp,wp2,if_slow):
         wps = MarkerArray()
         i=0
         for wpi in [wp,wp2]:
@@ -97,11 +106,22 @@ class UAVController(object):
             locwp.scale.x = 0.25
             locwp.scale.y = 0.25
             locwp.scale.z = 0.25
+            if if_slow:
+                locwp.scale.x = 0.35
+                locwp.scale.y = 0.35
+                locwp.scale.z = 0.35
+          
             if i == 1:
-                locwp.color.a = 1
-                locwp.color.r = 1
-                locwp.color.g = 0
-                locwp.color.b = 0.0   
+                if if_slow:
+                    locwp.color.a = 1
+                    locwp.color.r = 1.0
+                    locwp.color.g = 0
+                    locwp.color.b = 0
+                else:
+                    locwp.color.a = 1
+                    locwp.color.r = 0
+                    locwp.color.g = 0
+                    locwp.color.b = 1.0   
             else:
                 locwp.color.a = 1
                 locwp.color.r = 0
@@ -179,6 +199,7 @@ class UAVController(object):
             pose.pose.position.z = d[2]
 
             path.poses.append(pose)
+
         self.predtrj_pub.publish(path)
     def publish_stpath(self, data):
         path = Path()
@@ -221,14 +242,16 @@ if __name__ == '__main__':
     
     local_pos = controller.ros_data["local_position"]
     # set_points = np.array([local_pos,[20,20,4],[16,4,3],[-20,-18,2],[-18,6,3],[0,0,2]]) # the pre-assigned waypoints
-    set_points = np.array([local_pos,[50,0,4],[50,4,3],[0,4,2],[0,8,3],[50,8,2],[50,12,4],[0,12,4],[0,0,4]]) 
+    # set_points = np.array([local_pos,[50,0,4],[50,4,3],[0,4,2],[0,8,3],[50,8,2],[50,12,4],[0,12,4],[0,0,4]]) 
+    set_points = np.array([local_pos,[25,0,4],[50,0,4],[50,25,3],[0,25,2],[50,0,3],[50,50,2],[50,0,4],[0,0,4]])
+    # set_points = np.array([local_pos,[25,0,4],[50,0,4],[0,0,4]])
     # set_points[:,0:2] = set_points[:,0:2]*2
     mn_speed = 0.3 #not the constrain
     max_accel = 6
-    speed_range = [1.5,4,9]
-    spd_angle_shrd = [math.pi/6,math.pi*5/6]
+    speed_range = [1.5,6,9]
+    spd_angle_shrd = [math.pi/6,math.pi*9/10] #,math.pi*5/6
     accel_d = 0.1
-    decel_d = 0.3
+    decel_d = 0.2
     m_speed = 1 # start the flight from a lower speed
     slen=0.9
     rds = 3.5
@@ -268,7 +291,7 @@ if __name__ == '__main__':
                 local_pos = controller.ros_data["local_position"]
             rais+=1
         
-        if np.linalg.norm(set_points[-1]-local_pos)<2 and ct_goal>20:
+        if np.linalg.norm(set_points[-1]-local_pos)<2 and nst_tn_pt>1:
             for ii in range(100):
                 controller.set_vel(0,0,0,0)
             for iii in range(100):
@@ -276,13 +299,13 @@ if __name__ == '__main__':
             print("goal reached !!")
         else:
             y = controller.eular[2]
-            vel,dyaw,yaw,pred_trj,ct_goal,time_pred,t2,m_speed,lt_ctrl,pos_wp,pos_wp2,nst_tn_pt = Get_control.get_vel(set_points,
+            vel,dyaw,yaw,pred_trj,ct_goal,time_pred,t2,m_speed,lt_ctrl,pos_wp,pos_wp2,nst_tn_pt,if_slow = Get_control.get_vel(set_points,
                                               local_pos,ct_goal,waypoints_cv,slen,t2,time_pred,uav_vel,mn_speed,
                                               lc_max_speed,speed_range,m_speed,max_accel,pred_coe,lt_ctrl,y,turn_points,
                                               yaw_gnum,accel_d,decel_d,decel_rad,nst_tn_pt)
             # vel = np.array([0.1,0.1,0.1])
             controller.set_vel(vel[0],vel[1],vel[2],dyaw)
-            print('set_vel,ct_goal,waypoints_cv[ct_goal]',vel,ct_goal,waypoints_cv[ct_goal])
+            print('set_vel,ct_goal,waypoints_cv[ct_goal]',vel,ct_goal,waypoints_cv[np.clip(ct_goal,0,len(waypoints_cv)-1)])
         
         path.append(local_pos)
         if len(path)>200:
@@ -290,8 +313,8 @@ if __name__ == '__main__':
         controller.publish_path(path)
         controller.publish_stpath(set_points)
         # controller.publish_cvpath(waypoints_cv)
-        controller.publish_bp(waypoints_cv,turn_points[nst_tn_pt+1])
-        controller.publish_locwp(pos_wp+local_pos,pos_wp2+local_pos)
+        controller.publish_bp(waypoints_cv,turn_points,nst_tn_pt+1)
+        controller.publish_locwp(pos_wp+local_pos,pos_wp2+local_pos,if_slow)
         controller.publish_predtrj(pred_trj)
                 
         rospy.sleep(0.02)
